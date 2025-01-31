b6277ca96eed26b728bd00cc0a10a0554922f909
broute2 <charles.brouillette100@gmail.com>
2025-01-31 16:13:03 -0500

    exercise 5
    exercice 5

---
+++ execise 5.py
@@ -0,0 +1,5 @@
+""""""Est-ce qu'il fait froid?""""""'
+"""si oui alllumer le thermostat, si non ne rien faire""""""'
+""""""si le thermostat est allumé, afficher la température""""""'
+""""""si la température est inférieure à 20  allumer le chauffage""""
+""""""si la température est supérieure à 20 éteindre le chauffage et le thermostat""""""'
